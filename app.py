3e99c0004f2bd84480d470c78b98063711e2ad80
Aakash Kuragayala <st125050@ait.ac.th>
2024-09-28 10:40:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import pickle
 from sklearn.preprocessing import StandardScaler
+from mainfile import RidgeLogisticRegression  # Import the class
 
 # Load the trained Ridge Logistic Regression model
 with open('Ridge_Logistic_Regression_Model.pkl', 'rb') as file:
